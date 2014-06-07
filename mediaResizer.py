a60336de9e2840690435da2f7b7c669cf3783663
Jarrod Reuter <jarrod.reuter@gmail.com>
2014-06-06 23:02:05 -0400

    Basic docopt and some logging.

---
+++ mediaResizer.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Copyright 2014 Jarrod Reuter
+
+"""
+Program to process images and videos to desired format, resolution, and size.
+
+Usage:
+    mediaResizer [options] <folder>
+    mediaResizer -h | --help
+    mediaResizer --version
+
+Options:
+    -h --help       Show this screen.
+    --version       Show version.
+    -q              Quiet the logging to only ERROR level.
+    -v              Verbose output (INFO level).
+    --debug         Very Verbose output (DEBUG level).
+"""
+from docopt import docopt
+import logging
+import os
+
+
+class MediaResizerException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
+
+class MediaResizer:
+    _arguments = None
+    _log_level = 'WARN'
+
+    def __init__(self):
+        self._arguments = docopt(__doc__, version='0.1')
+        self._set_logging_verbosity()
+
+    def _set_logging_verbosity(self):
+        """
+        Sets the logging level based on arguments passed in the cli.
+        """
+        if self._arguments['-v']:
+            self._log_level = logging.INFO
+        if self._arguments['--debug']:
+            self._log_level = logging.DEBUG
+        if self._arguments['-q']:
+            self._log_level = logging.ERROR
+        logging.basicConfig(level=self._log_level,
+                            format='%(asctime)s %(message)s')
+
+    def main(self):
+        logging.info('Directory added: %s', self._arguments['<folder>'])
+
+        if os.path.basename(self._arguments['<folder>']).startswith('.'):
+            logging.info('Ignoring dot files.')
+            exit()
+
+
+if __name__ == '__main__':
+    MediaResizer().main()
