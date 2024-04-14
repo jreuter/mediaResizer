6a88834be47c0ee2d7a2eccb78b22f447627ed85
Jarrod Reuter <jarrod.reuter@gmail.com>
2024-04-14 03:17:53 -0400

    Timestamp fix is working.
    May add time delta option soon.

--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -4,9 +4,11 @@ set -e
 
 sudo apt-get install -y python3-pip
 
-pip3 install --user python-magic gexiv2-2
+pip3 install --user python-magic gexiv2-2 ffmpeg-python
 
-sudo apt-get install -y libgexiv2-2
+# python3-pymediainfo is for getting creation date in video files.
+# libgexiv2-2 is to get metadata from images.
+sudo apt-get install -y libgexiv2-2 python3-pymediainfo
 
 # Install Handbrake CLI
 sudo add-apt-repository ppa:stebbins/handbrake-releases
